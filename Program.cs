31b5b71c9b7193a872631b867ca67dd9ae1e74aa
梁君龙 <junlong.liang@gmail.com>
2019-05-28 15:23:53 +0800

    ### Commit test.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
